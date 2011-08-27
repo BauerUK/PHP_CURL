aee1f3aeca5b64b69755572af19266243e03155d
Bauer <bauer@glassoforange.co.uk>
2011-08-28 00:12:05 +0100

    Add: simple examples

---
+++ example/example.php
@@ -0,0 +1,19 @@
+<?php
+
+    include("../CURL.php");
+    
+    // Example 1. Grab some JSON
+    $curl = CURL::init("http://www.reddit.com/r/php/.json")
+            ->setReturnTransfer(TRUE);
+    
+    var_dump(json_decode($curl->execute()));
+    
+    // Example 2. Use our existing CURL object, and retrieve some headers
+    $headers = 
+        $curl->setHeader(TRUE)
+            ->setNobody(TRUE)
+            ->setReturnTransfer(TRUE)
+            ->execute();
+    
+    print $headers;
+    
